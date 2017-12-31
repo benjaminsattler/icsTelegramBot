35f490f9473aac8fa32d260d1ebd2c04cd27b967
kaktus <kaktuskuchen@gmail.com>
2017-12-31 11:36:42 +0100

    create class server

--- src/main.rb
+++ src/main.rb
@@ -3,18 +3,12 @@
 require 'yaml'
 require 'i18n'
 
-require_relative './data'
-require_relative './bot'
-require_relative './ics'
-require_relative './watchdog'
+require_relative './server'
 require_relative './log'
 
 $defaultConfigFile = File.join File.dirname(__FILE__), '..', 'config', 'conf.yml'
 
 $config = nil
-$events = nil
-$data = nil
-$bot = nil
 
 def loadConfig(configFile)
     YAML.load_file(configFile)
@@ -44,65 +38,8 @@ I18n.backend.load_translations
 I18n.default_locale = :de
 I18n.locale = locale unless locale.nil?
 
-$data = DataStore.new(File.join(File.dirname(__FILE__), '..', $config['db_path']))
-$events = ICS::Calendar.new
-$events.loadEvents(ICS::FileParser::parseICS(File.join(File.dirname(__FILE__), '..', $config['ics_path'])))
-$bot = Bot.new($config['bot_token'], $data, $events)
+$server = Server.new($config)
 
-$watchdog = Watchdog.new
-$eventThread = nil
-$databaseThread = nil
-$botThread = nil
-
-$eventThreadBlock  = lambda do
-    while(not Thread.current[:stop]) do
-        $events.getEvents.each do |event|
-            $bot.notify(event)
-        end
-        sleep 1
-    end
-end
-
-$databaseThreadBlock = lambda do
-    run = true
-    while(run) do
-        seconds = $config['flush_interval']
-        while(seconds > 0 && run) do
-            sleep 1
-            seconds = seconds - 1
-            run = false if Thread.current[:stop]
-        end
-        log("Syncing database...")
-        $data.flush
-        log("Syncing done...")
-    end
-end
-
-$botThreadBlock = lambda do
-    $bot.run
-end
-
-
-$watchdog.watch([{
-    name: 'Bot-Thread',
-    thr: $botThreadBlock
-}, {
-    name: 'Database-Thread',
-    thr: $databaseThreadBlock
-}, {
-    name: 'Event-Thread',
-    thr: $eventThreadBlock
-}])
-
-$execute = true
-Signal.trap("TERM") do
-    $execute = false
-    log("Shutdown signal received")
-    $watchdog.stop
-end
-
-while($execute) do
-    sleep 1
-end
+$server.run
 
 log("Shutdown complete.")
---
+++ src/server.rb
@@ -0,0 +1,78 @@
+require_relative './data'
+require_relative './bot'
+require_relative './ics'
+require_relative './watchdog'
+require_relative './log'
+
+class Server
+
+    attr_reader :options
+    def initialize(options)
+        @options = options
+    end
+
+    def run
+        $data = DataStore.new(File.join(File.dirname(__FILE__), '..', @options['db_path']))
+        $events = ICS::Calendar.new
+        $events.loadEvents(ICS::FileParser::parseICS(File.join(File.dirname(__FILE__), '..', @options['ics_path'])))
+        $bot = Bot.new(@options['bot_token'], $data, $events)
+        
+        $watchdog = Watchdog.new
+        $eventThread = nil
+        $databaseThread = nil
+        $botThread = nil
+        
+        $eventThreadBlock  = lambda do
+            while(not Thread.current[:stop]) do
+                $events.getEvents.each do |event|
+                    $bot.notify(event)
+                end
+                sleep 1
+            end
+        end
+        
+        $databaseThreadBlock = lambda do
+            run = true
+            while(run) do
+                seconds = @options['flush_interval']
+                while(seconds > 0 && run) do
+                    sleep 1
+                    seconds = seconds - 1
+                    run = false if Thread.current[:stop]
+                end
+                log("Syncing database...")
+                $data.flush
+                log("Syncing done...")
+            end
+        end
+        
+        $botThreadBlock = lambda do
+            $bot.run
+        end
+        
+        
+        $watchdog.watch([{
+            name: 'Bot-Thread',
+            thr: $botThreadBlock
+        }, {
+            name: 'Database-Thread',
+            thr: $databaseThreadBlock
+        }, {
+            name: 'Event-Thread',
+            thr: $eventThreadBlock
+        }])
+
+        $execute = true
+        Signal.trap("TERM") do
+            $execute = false
+            log("Shutdown signal received")
+            $watchdog.stop
+        end
+
+        while($execute) do
+            sleep 1
+        end
+        
+    end
+    
+end
