2d4bf56ebc509034de77ba9e210db97a245cdada
kaktus <kaktuskuchen@gmail.com>
2017-12-31 12:50:48 +0100

    * move i18n config to server

--- src/main.rb
+++ src/main.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
-require 'i18n'
 
 require 'server'
 require 'log'
@@ -29,17 +28,9 @@ end
  
 configFilename = getCommandlineArgument('--config').nil? ? defaultConfigFile : getCommandlineArgument('--config')
 config = loadConfig(configFilename)
-locale = getCommandlineArgument('--lang').to_sym unless getCommandlineArgument('--lang').nil?
-
-Logger.setLogfile(File.join(File.dirname(__FILE__), '..', config['log_file']))
-
-I18n.load_path = Dir[File.join(File.dirname(__FILE__), '..', 'lang', '*.yml')]
-I18n.backend.load_translations
-I18n.default_locale = :de
-I18n.locale = locale unless locale.nil?
+config['locale'] = ENV['LOCALE'] unless ENV['locale']
 
 server = Server.new(config)
-
 server.run
 
 log("Shutdown complete.")
--- src/server.rb
+++ src/server.rb
@@ -4,11 +4,20 @@ require 'ics'
 require 'watchdog'
 require 'log'
 
+require 'i18n'
+
 class Server
 
     attr_reader :options
     def initialize(options)
         @options = options
+
+        I18n.load_path = Dir[File.join(File.dirname(__FILE__), '..', 'lang', '*.yml')]
+        I18n.backend.load_translations
+        I18n.default_locale = :de
+        I18n.locale = options['locale'] unless options['locale'].nil?
+
+        Logger.setLogfile(File.join(File.dirname(__FILE__), '..', options['log_file']))        
     end
 
     def run
