4bb23107c0f839f937d12b8b906793ad70efaba9
kaktus <kaktuskuchen@gmail.com>
2017-12-31 15:00:03 +0100

    * add log and pid commandline options to server script

--- src/log.rb
+++ src/log.rb
@@ -3,13 +3,7 @@ def log(msg)
 end
 
 class Logger
-    def self.setLogfile(filename)
-        $stdout.reopen(filename, 'a')
-        $stderr = $stdout        
-    end
-
-    def self.log(msg)
+     def self.log(msg)
         puts "#{Time.now.strftime('%d.%m.%Y %H:%M:%S')} (#{caller.first}): #{msg}"
-        STDOUT.flush
     end
 end
--- src/server.rb
+++ src/server.rb
@@ -5,6 +5,7 @@ require 'watchdog'
 require 'log'
 
 require 'i18n'
+require 'fileutils'
 
 class Server
 
@@ -16,8 +17,83 @@ class Server
         I18n.backend.load_translations
         I18n.default_locale = :de
         I18n.locale = options['locale'] unless options['locale'].nil?
+        #Logger.setLogfile(File.join(File.dirname(__FILE__), '..', options['log_file']))        
+    end
 
-        Logger.setLogfile(File.join(File.dirname(__FILE__), '..', options['log_file']))        
+    def pidfile?
+        !@options['pid'].nil?
+    end
+
+    def pidfile
+        @options['pid']
+    end
+
+    def logfile?
+        !@options['log'].nil?
+    end
+
+    def logfile
+        @options['log']
+    end
+
+    def redirect_output(filename)
+        FileUtils.mkdir_p(File.dirname(filename), :mode => 0755)
+        FileUtils.touch filename
+        File.chmod(0644, filename)
+        $stderr.reopen(filename, 'a')
+        $stdout.reopen($stderr)
+        $stdout.sync = $stderr.sync = true
+    end
+
+    def status_from_pidfile
+        return :dead unless self.pidfile?
+        begin
+            pid = File.read(self.pidfile).to_i
+            return :dead if pid == 0
+            return :running unless pid == 0
+        rescue Errno::EPERM, Errno::EACCES
+            return :running
+        rescue Errno::ENOENT
+            return :dead
+        end
+    end
+
+    def write_pidfile
+        FileUtils.mkdir_p(File.dirname(self.pidfile), :mode => 0755)
+        FileUtils.touch self.pidfile
+        begin
+            pid = File.open(self.pidfile, 'w') { |f| f.write(Process.pid) }
+        rescue Errno::EPERM
+            puts "Cannot write PIDFILE #{self.pidfile}: Permission denied!"
+            exit
+        end
+    end
+
+    def check_running
+        status = self.status_from_pidfile
+        case status
+        when :dead
+            begin
+                File.delete(self.pidfile)
+            rescue Errno::EPERM, Errno::EACCES
+                puts "Cannot delete PIDFILE #{self.pidfile}: Permission error!"
+                exit
+            rescue Errno::ENOENT
+            end
+        when :running
+            puts "Server is already running! If you think this is a mistake, please delete pidfile #{File.expand_path(self.pidfile)}"
+            exit
+        end
+    end
+
+    def start
+        self.redirect_output(self.logfile) if self.logfile?
+        self.check_running if self.pidfile?
+        self.write_pidfile if self.pidfile?
+        puts "logtest"
+        while(true) do 
+            sleep 1
+        end
     end
 
     def run
