49c9f8f0ac8513baee8ca2ef8fbcd65f97f84292
kaktus <kaktuskuchen@gmail.com>
2018-01-01 20:57:56 +0100

    show real source of log messages

--- src/log.rb
+++ src/log.rb
@@ -4,6 +4,6 @@ end
 
 class Logger
      def self.log(msg)
-        puts "#{Time.now.strftime('%d.%m.%Y %H:%M:%S')} (#{caller.first}): #{msg}"
+        puts "#{Time.now.strftime('%d.%m.%Y %H:%M:%S')} (#{caller[1]}): #{msg}"
     end
 end
