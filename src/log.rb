2e0cfd91b745cb53693630862ded376db5fb2f56
kaktus <kaktuskuchen@gmail.com>
2018-01-06 18:17:55 +0100

    * respond to signal USR1 by reopening logfile

--- src/log.rb
+++ src/log.rb
@@ -5,5 +5,8 @@ end
 class Logger
      def self.log(msg)
         puts "#{Time.now.strftime('%d.%m.%Y %H:%M:%S')} (#{caller[1]}): #{msg}"
+        if $stdout.closed?
+            $stdout.reopen(filename, 'a');
+        end
     end
 end
