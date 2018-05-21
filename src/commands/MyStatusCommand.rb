909fd2cb0158d54cb739462bf570cda1efaa67c9
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-05-21 16:52:20 +0200

    Reduce message spam in mystatus command

--- src/commands/MyStatusCommand.rb
+++ src/commands/MyStatusCommand.rb
@@ -14,17 +14,19 @@ class MyStatusCommand < Command
             @messageSender.process(I18n.t('status.not_subscribed'), chatid)
             return
         end
-        @messageSender.process(I18n.t('status.intro'), chatid)
+        text = Array.new
+        text << I18n.t('status.intro')
         subscriptions.each do |subscription|
             reminder_time = "#{pad(subscription[:notificationtime][:hrs], 2)}:#{pad(subscription[:notificationtime][:min], 2)}"
             calendar_name = calendars[subscription[:eventlist_id]][:description]
             if subscription[:notificationday] == 0 then
-                @messageSender.process(I18n.t('status.subscribed_sameday', reminder_time: reminder_time, calendar_name: calendar_name), chatid)
+                text << I18n.t('status.subscribed_sameday', reminder_time: reminder_time, calendar_name: calendar_name)
             elsif subscription[:notificationday] == 1 then
-                @messageSender.process(I18n.t('status.subscribed_precedingday', reminder_time: reminder_time, calendar_name: calendar_name), chatid)
+                text << I18n.t('status.subscribed_precedingday', reminder_time: reminder_time, calendar_name: calendar_name)
             else
-                @messageSender.process(I18n.t('status.subscribed_otherday', reminder_day_count: subscription[:notificationday], reminder_time: reminder_time, calendar_name: calendar_name), chatid)
+                text << I18n.t('status.subscribed_otherday', reminder_day_count: subscription[:notificationday], reminder_time: reminder_time, calendar_name: calendar_name)
             end
         end
+        @messageSender.process(text.join("\n"), chatid)
     end
 end
