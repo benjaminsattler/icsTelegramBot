0fffa522e4d38efe374bea0d5199933f73259a1d
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-07-23 21:57:24 +0200

    split deploy.sh into release.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-BASE_DIR=`dirname $BASH_SOURCE`/../
 source ${SCRIPT_DIR}config/deployment.sh
+IFS='' read -r -d '' SSH_COMMAND <<EOT
+echo dir is $sshlocaldir
+cd $sshlocaldir
+git fetch --tags
+git checkout master
+bundle install
+ICSBOT_ENV=production ./scripts/migrate-database.sh migrate
+./scripts/restart-bot-production.sh
+EOT
 
-ssh -t $sshurl "cd ${sshlocaldir} && git fetch && git checkout master && git merge -S --no-ff origin/development && git push origin master && bundle install && ICSBOT_ENV=production ./scripts/migrate-database.sh migrate && ./scripts/restart-bot-production.sh"
+ssh $sshurl -i $sshkeyfile -t "${SSH_COMMAND}"
