a1e06b026e6840a8f73c4dc35acdf72004252aea
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-09-01 19:03:17 +0200

    Remove migration container after run

--- scripts/migrate-database.sh
+++ scripts/migrate-database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source `dirname $BASH_SOURCE`/../docker/migrations/docker.env
-docker-compose run migrations --migrations-dir $MIGRATIONS_DIR $*
+docker-compose run --rm migrations --migrations-dir $MIGRATIONS_DIR $*
