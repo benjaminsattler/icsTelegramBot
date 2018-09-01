1fa99b9428f7de411753e75cda6dbdbdbbc9cd53
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-09-01 21:16:43 +0200

    Add dbmate docker image to compile script

--- scripts/compile-docker.sh
+++ scripts/compile-docker.sh
@@ -2,8 +2,11 @@
 
 echo "This script builds some docker images that are required for rspec and rubocop."
 
-echo "1/2: Build rubocop image"
+echo "1/3: Build rubocop image"
 docker build -t muell_rubocop --rm -f docker/rubocop/Dockerfile $PWD
 
-echo "2/2: Build rspec image"
+echo "2/3: Build rspec image"
 docker build -t muell_rspec --rm -f docker/rspec/Dockerfile $PWD
+
+echo "3/3: Build dbmate image"
+docker build -t muell_dbmate --rm -f docker/migrations/Dockerfile $PWD
--- scripts/migrate-database.sh
+++ scripts/migrate-database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source `dirname $BASH_SOURCE`/../docker/migrations/docker.env
-docker-compose run --rm migrations --migrations-dir $MIGRATIONS_DIR $*
+docker run --rm -v $PWD/db/:/db --env-file $PWD/docker/migrations/docker.env --network=muell_backend muell_dbmate --migrations-dir $MIGRATIONS_DIR $*
