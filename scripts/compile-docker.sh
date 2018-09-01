0f631dd96c564857ab5992a5e2d0391b144dbc86
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-09-01 21:04:31 +0200

    Fix rubocop and rspec dockerfiles

--- scripts/compile-docker.sh
+++ scripts/compile-docker.sh
@@ -3,7 +3,7 @@
 echo "This script builds some docker images that are required for rspec and rubocop."
 
 echo "1/2: Build rubocop image"
-docker build -t muell_rubocop --rm $PWD/docker/rubocop
+docker build -t muell_rubocop --rm -f docker/rubocop/Dockerfile $PWD
 
 echo "2/2: Build rspec image"
-docker build -t muell_rspec --rm $PWD/docker/rspec
+docker build -t muell_rspec --rm -f docker/rspec/Dockerfile $PWD
