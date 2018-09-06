f284fa59997c1ef77f93fe5b7fce718f2b29ff1f
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-09-06 20:31:02 +0200

    Delete unused files, add dev image build

--- scripts/compile-docker.sh
+++ scripts/compile-docker.sh
@@ -2,14 +2,17 @@
 
 echo "This script builds some docker images that are required for rspec and rubocop."
 
-echo "1/4: Build production image"
+echo "1/5: Build production image"
 docker build -t muell --rm -f docker/app-production/Dockerfile $PWD
 
-echo "2/4: Build rubocop image"
+echo "2/5: Build development image"
+docker build -t muell_dev --rm -f docker/app-devel/Dockerfile $PWD
+
+echo "3/5: Build rubocop image"
 docker build -t muell_rubocop --rm -f docker/rubocop/Dockerfile $PWD
 
-echo "3/4: Build rspec image"
+echo "4/5: Build rspec image"
 docker build -t muell_rspec --rm -f docker/rspec/Dockerfile $PWD
 
-echo "4/4: Build dbmate image"
+echo "5/5: Build dbmate image"
 docker build -t muell_dbmate --rm -f docker/migrations/Dockerfile $PWD
