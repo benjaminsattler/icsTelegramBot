b489af2f3012f85e23aa71d7ca3211f3a5483ea9
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-09-15 22:33:56 +0200

    Enable debugger conditionally in dev image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 dbmate wait && echo "Migrating..." && dbmate --no-dump-schema --migrations-dir ${MIGRATIONS_DIR} up && echo "Migration complete"
-rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- $@
+if [ "$ENABLE_DEBUG" == "1" ]; then
+  rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- $@
+else
+  exec $@
+fi
