223076724e4d6f8687fd62118314f33ee087791c
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-12-09 00:33:45 +0100

    update travis config

--- scripts/ci/before_deploy.sh
+++ scripts/ci/before_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O hypercli.tar.gz https://github.com/hyperhq/hypercli/archive/v1.10.16.tar.gz && tar xvvf hypercli.tar.gz && cd hypercli-1.10.16 && chmod +x ./build.sh && source ./build.sh
+wget -O hypercli.tar.gz https://github.com/hyperhq/hypercli/archive/v1.10.16.tar.gz && tar xvvf hypercli.tar.gz && chmod -R 777 hypercli-1.10.16 && cd hypercli-1.10.16 && chmod +x ./build.sh && ./build.sh
