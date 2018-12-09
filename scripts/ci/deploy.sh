f8b590cafd08483289a8f6b28f21fc61197d7bd1
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-12-09 15:22:31 +0100

    update travis config

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && rake docker:build_push_prod && rake hyper:update
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && \
+rake docker:build_push_prod && \
+rake hyper:update
