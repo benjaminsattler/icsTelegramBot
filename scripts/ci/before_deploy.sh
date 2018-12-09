f43d9052339dc25fe6d1680f66be908a5793da9e
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-12-09 15:06:41 +0100

    update travis config

--- scripts/ci/before_deploy.sh
+++ scripts/ci/before_deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-wget -O hyper.tar.gz https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz && tar xvvf hyper.tar.gz && sudo mv hyper /usr/local/bin && hyper --version
+openssl aes-256-cbc -K $encrypted_31ee9ec10cd4_key -iv $encrypted_31ee9ec10cd4_iv -in production.env.enc -out docker/production.env -d && \
+wget -O hyper.tar.gz https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz && \
+tar xvvf hyper.tar.gz && \
+sudo mv hyper /usr/local/bin && \
+hyper --version
