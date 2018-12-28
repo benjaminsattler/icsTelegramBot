fb0fd64c438936724ae966d75ac6c0eeb5f3e3a4
Benjamin Sattler <bsattler.inbox@gmail.com>
2018-12-28 19:19:47 +0100

    Make debugger port configurable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -24,7 +24,7 @@ if [[ "$ICSBOT_ENV" == "development" ]]; then
 fi
 echo "Migrating..." && dbmate --no-dump-schema --migrations-dir ${MIGRATIONS_DIR} up && echo "Migration complete"
 if [[ "$ENABLE_DEBUG" == "1" && "$ICSBOT_ENV" == "development" ]]; then
-  rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- $@
+  rdebug-ide --host 0.0.0.0 --port $DEBUGGER_PORT --dispatcher-port 26162 -- $@
 else
   exec $@
 fi
