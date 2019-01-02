810ceea9b28c800f6d6906258cf97197de143f7e
Benjamin Sattler <bsattler.inbox@gmail.com>
2019-01-02 21:55:24 +0100

    enable gcloud path autocompletion

--- scripts/ci/before_deploy.sh
+++ scripts/ci/before_deploy.sh
@@ -2,6 +2,7 @@
 
 set -ev
 curl https://sdk.cloud.google.com | bash > /dev/null
+source $HOME/google-cloud-sdk/path.bash.inc
 gcloud --quiet components list
 gcloud --quiet components update
 gcloud --quiet components install beta kubectl docker-credential-gcr
