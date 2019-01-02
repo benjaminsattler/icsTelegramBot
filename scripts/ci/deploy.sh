44675289139dd99bb3c289f907fcfbc4c6be5a8f
Benjamin Sattler <bsattler.inbox@gmail.com>
2019-01-02 22:30:33 +0100

    change how deploy script gets git tag

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -2,7 +2,7 @@
 
 set -ev
 source $HOME/google-cloud-sdk/path.bash.inc
-git_tag=`git describe --tags | head -n 1`
+git_tag=`git tag | tail -n 1`
 gce_image_name="${GCE_REGISTRY_HOST}/${K8S_PROJECT_NAME}/${GCE_DOCKER_IMAGE_NAME}"
 
 echo $GCE_DOCKER_LOGIN > k8s/secrets/gce.json
