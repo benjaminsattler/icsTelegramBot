7269ffac7aa6f999e6a9f476051ebe3f1a56b78d
Benjamin Sattler <bsattler.inbox@gmail.com>
2019-01-20 18:17:15 +0100

    Adapt ci scripts to new workflow

--- scripts/ci/before_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-curl https://sdk.cloud.google.com | bash > /dev/null
-source $HOME/google-cloud-sdk/path.bash.inc
-gcloud --quiet components list
-gcloud --quiet components update
-gcloud --quiet components install beta kubectl docker-credential-gcr
-gcloud --quiet version
